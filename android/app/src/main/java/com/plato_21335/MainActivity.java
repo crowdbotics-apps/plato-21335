e80e92429600a72f5216127fc4eac285688fa4ea
Crowdbotics <team@crowdbotics.com>
2020-10-10 17:23:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/plato_21335/MainActivity.java
@@ -0,0 +1,15 @@
+package com.plato_21335;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "plato_21335";
+    }
+}
